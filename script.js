0c74312331a949dae7ba7e547c64626fa27b0131
Alan Gonçalves <biscanas4@gmail.com>
2021-08-12 13:25:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function contar(){
+    var inicio = document.querySelector('#inicio')
+    var fim = document.querySelector('fim')
+    var passo = document.querySelector('passo')
+
+    var divi = document.body.createElement('div')
+    div.setAttribute('id', 'res')
+    res.innerHTML = 'Contando'
+
+    /*for(inicio.value;inicio<=fim;inicio+=passo){
+        res.innerHTML += inicio
+    }*/
+}
